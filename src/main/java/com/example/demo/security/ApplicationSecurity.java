6fde5b0dede93659e7326dbf454710db5f93a521
farrukh.ahmed <farrukh.ahmed@venturedive.com>
2020-06-14 01:04:44 +0500

    update on security

--- src/main/java/com/example/demo/security/ApplicationSecurity.java
+++ src/main/java/com/example/demo/security/ApplicationSecurity.java
@@ -11,6 +11,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.Permissions.*;
 import static com.example.demo.security.Roles.ADMIN;
@@ -25,9 +28,10 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable()
+        http
+                .csrf().disable()//.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .authorizeRequests()
-                .antMatchers("/")
+                .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
                 .antMatchers("/api/v1/students/**").hasRole(STUDENT.name())
                 .antMatchers(HttpMethod.POST, "/api/v1/management/**").hasAuthority(STUDENT_WRITE.getPermission())
@@ -36,7 +40,26 @@ public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET, "/api/v1/management/**").hasAnyRole(STUDENT.name(), ADMIN.name())
                 .anyRequest()
                 .authenticated()
-                .and().httpBasic();
+                .and()
+                //.httpBasic();
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/welcome", true)
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                //.rememberMe();
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30))
+                    .rememberMeParameter("rememberme")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me", "XSRF-TOKEN")
+                    .logoutSuccessUrl("/login");
     }
 
     @Override
